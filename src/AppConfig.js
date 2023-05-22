d8693f02d388e62785a3168a6ceac831e823fbe7
consuti-tec <consuti@consuti.com.br>
2023-05-22 00:21:01 -0300

    first commit

---
+++ src/AppConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+  ColorTheme: "#ffffff",
+  UrlPortal: "https://escoladoesporte.com.br/redesporti/unidades/bach-auer-assessoria-e-servicos-esportivos-ltda",
+  UrlPainel: "https://painel.escoladoesporte.com.br/",
+  TimeSplashScreen: 3000,
+};
